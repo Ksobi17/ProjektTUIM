a748566168a15d812a852b66a4dd1a653cb8b820
Ksobi17 <157920191+Ksobi17@users.noreply.github.com>
2025-01-16 03:14:21 +0100

    Delete server directory

--- server/src/main/java/com/wizyta/server/jpa/DoctorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wizyta.server.jpa;
-
-import com.wizyta.server.data.Doctor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DoctorRepository extends JpaRepository<Doctor, Long> {
-}
